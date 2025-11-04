96f0303d267e0e74887ca0510fa8fe37b87cf9e3
Brandon D'Agostino <bdag@stanford.edu>
2025-11-03 16:04:40 -0800

    Added raw data and data loading-cleaning pipeline.

--- src/cs_238/pipelines/data_loader/nodes.py
+++ src/cs_238/pipelines/data_loader/nodes.py
@@ -2,3 +2,50 @@
 This is a boilerplate pipeline 'data_loader'
 generated using Kedro 1.0.0
 """
+
+import logging
+from collections.abc import Mapping, Sequence
+from typing import Final
+
+import pandas as pd
+from pydantic import BaseModel, Field
+
+_LOGGER: Final = logging.getLogger(__name__)
+
+
+class CleanParams(BaseModel):
+    dropna: bool = True
+    reset_index: bool = True
+    drop_columns: Sequence[str] = Field(default_factory=list)
+    column_dtypes: Mapping[str, str] = Field(default_factory=dict)
+    datetime_columns: Sequence[str] = Field(default_factory=list)
+
+
+def clean(raw: pd.DataFrame, clean_params: Mapping) -> pd.DataFrame:
+    params = CleanParams(**clean_params)
+    cleaned = raw
+
+    if params.dropna:
+        _LOGGER.debug("Dropping all-null columns")
+        cleaned = cleaned.dropna(axis=1, how="all")
+
+    if params.drop_columns:
+        _LOGGER.debug("Dropping columns: %s", params.drop_columns)
+        cleaned = cleaned.drop(
+            params.drop_columns,
+            axis=1,
+        )
+
+    if params.column_dtypes:
+        _LOGGER.debug("Casting columns to dtypes: %s", params.column_dtypes)
+        cleaned.astype(params.column_dtypes)
+
+    for col in params.datetime_columns:
+        _LOGGER.debug("Parsing datetime column: %s", col)
+        cleaned[col] = pd.to_datetime(cleaned[col])
+
+    if params.reset_index:
+        _LOGGER.debug("Resetting index")
+        cleaned = cleaned.reset_index(drop=True)
+
+    return cleaned
--- src/cs_238/pipelines/data_loader/pipeline.py
+++ src/cs_238/pipelines/data_loader/pipeline.py
@@ -7,6 +7,29 @@ from typing import Any
 
 from kedro.pipeline import Node, Pipeline
 
+from .nodes import clean
+
 
 def create_pipeline(**kwargs: Any) -> Pipeline:
-    return Pipeline([])
+    return Pipeline(
+        [
+            Node(
+                func=clean,
+                inputs=["raw_alarms", "params:data_loader.alarms"],
+                outputs="clean_alarms",
+                name="clean_alarms",
+            ),
+            Node(
+                func=clean,
+                inputs=["raw_charging_sessions", "params:data_loader.charging_sessions"],
+                outputs="clean_charging_sessions",
+                name="clean_charging_sessions",
+            ),
+            Node(
+                func=clean,
+                inputs=["raw_station_overviews", "params:data_loader.station_overviews"],
+                outputs="clean_station_overviews",
+                name="clean_station_overviews",
+            ),
+        ]
+    )
